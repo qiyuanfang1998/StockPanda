3a85f46bf335e3616e9544386fe4c1991352a9c1
qiyuanfang1998 <qiyuan.fang@stonybrook.edu>
2018-07-15 17:49:02 -0400

    Added template structure, bootstrap, and auto watchlist & super_portfolio - User autocreate

--- StockPanda/App/models.py
+++ StockPanda/App/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 '''
 Django ORM models for database for main Application.
 Information for queries are not stored.
 Description:
 Users have A SuperPortfolio
+Users have A WatchList
 SuperPortfolios have ZERO OR MORE Portfolios
 Portfolios MANY TO MANY ZERO OR MORE Stocks through StockOwnership intermediate
 Portfolios MANY TO MANY ZERO OR MORE CryptoCurrency through CryptoCurrencyOwnership intermediate
@@ -60,11 +63,25 @@ class CryptoCurrency(models.Model):
     def __str__(self):
         return self.ticker
 
+class WatchList(models.Model):
+    owned_by = models.OneToOneField(User,on_delete = models.CASCADE, related_name = 'watchlist')
+    stocks_watched = models.ManyToManyField(Stock, related_name = 'stocks_watched')
+    cryptocurrencies_watched = models.ManyToManyField(CryptoCurrency, related_name = 'cryptocurrencies_watched')
+
+    @receiver(post_save,sender = User)
+    def create_user_watchlist(sender,instance,created,**kwargs):
+        if created:
+            WatchList.objects.create(owned_by = instance)
+
+    @receiver(post_save, sender = User)
+    def save_user_watchlist(sender, instance, **kwargs):
+        instance.watchlist.save()
+
 class SuperPortfolio(models.Model):
     #super portfolio attributes
     total_value = models.DecimalField(max_digits = 19, decimal_places = 2)
     #super portfolio - User foreign key
-    owned_by = models.ForeignKey(User,on_delete = models.CASCADE, related_name = 'superportfolio')
+    owned_by = models.OneToOneField(User,on_delete = models.CASCADE, related_name = 'superportfolio')
     #aggregate portfolio perfomance
         #portfolio amount changes information
     one_day_performance_amount = models.DecimalField(max_digits = 19, decimal_places = 2,default = 0)
@@ -81,10 +98,19 @@ class SuperPortfolio(models.Model):
     one_year_performance_percent = models.DecimalField(max_digits = 19, decimal_places = 2,default = 0)
     all_time_performance_percent = models.DecimalField(max_digits = 19, decimal_places = 2,default = 0)
 
+    @receiver(post_save, sender = User)
+    def create_user_superportfolio(sender, instance, created, **kwargs):
+        if created:
+            SuperPortfolio.objects.create(owned_by = instance)
+
+    @receiver(post_save, sender = User)
+    def save_user_superportfolio(sender, instance, **kwargs):
+        instance.superportfolio.save()
+
 class Portfolio(models.Model):
     #portfolio attributes
     name = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 250)
     total_value = models.DecimalField(max_digits = 19, decimal_places = 2)
     cash = models.DecimalField(max_digits = 19, decimal_places = 2)
     #portfolio - super portfolio foreign key
