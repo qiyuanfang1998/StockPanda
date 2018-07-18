1bcfcfc284ff582d64c8728a6bac657e035f0d1a
qiyuanfang1998 <qiyuan.fang@stonybrook.edu>
2018-07-18 18:07:16 -0400

    Added exists function for querying db for stocks

--- StockPanda/App/stockapi/stockquery.py
+++ StockPanda/App/stockapi/stockquery.py
@@ -1,7 +1,13 @@
+from App.models import *
 
 '''
 This module determines if stock queried is a stock not yet in database -> addnewstock module
 or if the stock queried is already in the database -> updatestock module
 '''
 
-#todo
+def exists(symbol):
+    '''
+    Returns Boolean of stock with symbol = symbol exists in db
+    '''
+    return Stock.objects.filter(symbol = symbol).exists()
+
