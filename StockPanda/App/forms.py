cfe75c042af008bf69c057282024b1de0706f15c
qiyuanfang1998 <qiyuan.fang@stonybrook.edu>
2018-07-24 23:09:28 -0400

    Added form for editing account

--- StockPanda/App/forms.py
+++ StockPanda/App/forms.py
@@ -31,10 +31,50 @@ class SignUpForm(UserCreationForm):
             raise forms.ValidationError("This email is already in use")
         return data
 
-class AccountInformationChangeForm(forms.Form):
+class AccountInformationChangeForm(forms.ModelForm):
     '''
-    This form extends django.forms.Form
+    This form extends django.forms.ModelForm
     It's purpose is to allow the User to change account info
-    It is served in the account.html page with AJAX
+    It is served by the respective function in views.py to the account.html page with AJAX
+    asynchronously
     '''
-    pass
+    username = forms.CharField()
+    first_name = forms.CharField(max_length = 30, required = True)
+    last_name = forms.CharField(max_length = 150, required = True)
+    email = forms.CharField(max_length = 254, required = True, widget = forms.EmailInput())
+
+    class Meta:
+        model = User
+        fields = ('username','email','first_name','last_name')        
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(AccountInformationChangeForm, self).__init__(*args, **kwargs)
+
+
+    def save(self, commit = True):
+        user = self.request.user
+        if user.username == self.cleaned_data["username"]:
+            pass
+        else:
+            user.username = self.cleaned_data["username"]
+        
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_email["email"]
+        if commit :
+            user.save()
+        return user
+
+    # def clean_username(self):
+    #     username = self.cleaned_data['username']
+    #     if User.objects.filter(username = username).exists() and not username == self.request.user.username:
+    #         raise forms.ValidationError("This Username is already in use")
+    #     return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email = email).exists() and not email == self.request.user.email:
+            raise forms.ValidationError("This email is already in use")
+        return email
+
--- StockPanda/App/views.py
+++ StockPanda/App/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login as auth_login
-from .forms import SignUpForm
+from .forms import SignUpForm, AccountInformationChangeForm
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 # home-splashpage view
 def home(request):
+    '''
+    Redirect to home.html if user is not auth, overview.html if user is authenticated
+    '''
     if request.user.is_authenticated:
         return render(request,'overview.html')
     else:
@@ -71,12 +74,20 @@ def analyze(request):
 
 #account
 def account(request):
-    return render(request,'account.html')
+    if request.method == 'POST':
+        form = AccountInformationChangeForm(request.POST, request = request, initial = {'username': request.user.username , 'first_name': request.user.first_name, 'last_name': request.user.last_name,'email': request.user.email})
+        if form.is_valid():
+            user = form.save()
+            return redirect('account')
+    else:
+        form = AccountInformationChangeForm(request = request,initial = {'username': request.user.username , 'first_name': request.user.first_name, 'last_name': request.user.last_name,'email': request.user.email})
+
+    return render(request, 'account.html', {'form': form})
 
 #404 view
 def view_404(request):
     response = render_to_response('404.html', {},
-                              context_instance=RequestContext(request))
+    context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
